1109644ad023a5d0e8749bf0b51d51642f06dd39
Ronnie Geraghty <ronniegerag@gmail.com>
2019-05-26 02:20:49 -0400

    fixed and styled navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-﻿import React, { Component } from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+﻿import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
-import Blank from "./components/Blank";
-import Grid from "./components/Grid";
-import Blank2 from "./components/Blank2";
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+
 //TODO Web Template Studio: Add routes for your new pages here.
 class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar />
-        <Switch>
-          < Redirect exact path = "/" to = "/Blank" />
-          < Route path = "/Blank" component = { Blank } />
-          < Route path = "/Grid" component = { Grid } />
-          < Route path = "/Blank2" component = { Blank2 } />
-        </Switch>
-        <Footer />
-      </React.Fragment>
-    );
-  }
+	render() {
+		return (
+			<React.Fragment>
+				<NavBar />
+				<Switch>
+					<Redirect exact path="/" to="/Home" />
+					<Route path="/Home" component={Home} />
+					<Route path="/Dashboard" component={Dashboard} />
+				</Switch>
+				<Footer />
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,38 +1,33 @@
-﻿import React from "react";
-import styles from "./footer.module.css";
+﻿import React, { Component } from 'react';
+import styles from './footer.module.css';
 
-export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <div className="container-fluid">
-        <div className="row justify-content-around">
-          <div className="col-8 col-md-5">
-            <h5 className={styles.title}>HomeControl</h5>
-            <p className={styles.description}>
-              This is placeholder text. Your web app description goes here.
-            </p>
-          </div>
-          <div className="col-2">
-            <ul className="list-unstyled">
-              <li>
-                <a className={styles.footerlink} href="/">
-                  Example Link
-                </a>
-              </li>
-              <li>
-                <a className={styles.footerlink} href="/">
-                  Example Link
-                </a>
-              </li>
-              <li>
-                <a className={styles.footerlink} href="/">
-                  Example Link
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+class Footer extends Component {
+	render() {
+		return (
+			<footer className={styles.footer}>
+				<div className="container-fluid">
+					<div className="row justify-content-around">
+						<div className="col-8 col-md-5">
+							<h5 className={styles.title}>Home Control</h5>
+							<p className={styles.description}>A tool to control and monitor your house.</p>
+						</div>
+						<div className="col-2">
+							<ul className="list-unstyled">
+								<li>
+									<a
+										className={styles.footerlink}
+										href="https://github.com/ronniegeraghty/HomeControl"
+									>
+										Github
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</footer>
+		);
+	}
 }
+
+export default Footer;
